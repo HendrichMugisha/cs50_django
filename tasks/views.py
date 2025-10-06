7a149b89754e5d74892ca3143465f095fa819abf
Hendrichmugisha <hendrichmugisha@gmail.com>
2025-10-06 20:20:34 +0530

    completed the django course from cs50, wonderful one by this dude called brian Yu

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,35 @@
+from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
+tasks = []
 # Create your views here.
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=10)
+
+
 def index(request):
-    tasks = ['nice', 'not nice', 'nothing']
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     context = {
-        'tasks' : tasks
+        'tasks' : request.session["tasks"]
     }
     return render(request, 'tasks.html', context)
 
 def add_task(request):
-    return render(request, 'add_task.html')
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            request.session["tasks"] += [task]
+            # print(task)
+            return HttpResponseRedirect(reverse("tasks:home"))
+        else:
+            return render(request, 'add_task.html', {
+                "form" : form
+            })
+
+    return render(request, 'add_task.html', {"form": NewTaskForm()})
+        
